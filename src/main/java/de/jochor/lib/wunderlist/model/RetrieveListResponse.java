33a220b90def7c6e521894bbc4d12496aa8e2bcc
Jochen Hormes <jochen@jochor.de>
2015-10-25 21:15:45 +0100

    Added protocol to URIs. Added missing properties to RetrieveListResponse. #5

--- src/main/java/de/jochor/lib/wunderlist/model/RetrieveListResponse.java
+++ src/main/java/de/jochor/lib/wunderlist/model/RetrieveListResponse.java
@@ -7,17 +7,21 @@ import lombok.Setter;
  * json<br>
  * {<br>
  * "id": 83526310,<br>
- * "created_at": "2013-08-30T08:29:46.203Z",<br>
  * "title": "Read Later",<br>
+ * "owner_type":"user",<br>
+ * "owner_id":1235616<br>
  * "list_type": "list",<br>
- * "type": "list",<br>
+ * "public" : false,<br>
  * "revision": 10<br>
+ * "created_at": "2013-08-30T08:29:46.203Z",<br>
+ * "created_by_request_id" : "498d3ffc44ddfa2f275b:0fc81b85-31e0-4d09-b3f4-6af222..." ,<br>
+ * "type": "list",<br>
  * }
  *
  * <p>
  * <b>Started:</b> 2015-08-19
  * </p>
- * 
+ *
  * @author Jochen
  *
  */
@@ -27,14 +31,22 @@ public class RetrieveListResponse {
 
 	private int id;
 
-	private String created_at;
-
 	private String title;
+
+	private String owner_type;
+
+	private int owner_id;
 
 	private String list_type;
 
-	private String type;
+	private boolean Public; // upper case since 'public' is a key word
 
 	private int revision;
+
+	private String created_at;
+
+	private String created_by_request_id;
+
+	private String type;
 
 }
--- src/test/java/de/jochor/lib/wunderlist/model/RetrieveListResponseTest.java
+++ src/test/java/de/jochor/lib/wunderlist/model/RetrieveListResponseTest.java
@@ -14,22 +14,32 @@ import org.junit.Assert;
 public class RetrieveListResponseTest extends AbstractRoundtripTest<RetrieveListResponse> {
 
 	public static final int id = 83526310;
-	private static final String createdAt = "2013-08-30T08:29:46.203Z";
 	private static final String title = "Read Later";
+	private static final String ownerType = "user";
+	private static final int ownerID = 1639234;
 	private static final String listType = "list";
-	private static final String type = "list";
+	private static final boolean isPublic = false;
 	private static final int revision = 10;
+	private static final String createdAt = "2013-08-30T08:29:46.203Z";
+	private static final String created_by_request_id = "kaioutctphcpcp438qt983q498hqc4849";
+	private static final String type = "list";
 
 	@Override
 	protected RetrieveListResponse createEntity() {
-		RetrieveListResponse retrieveListResponse = new RetrieveListResponse();
-		retrieveListResponse.setId(id);
-		retrieveListResponse.setTitle(title);
-		retrieveListResponse.setCreated_at(createdAt);
-		retrieveListResponse.setList_type(listType);
-		retrieveListResponse.setType(type);
-		retrieveListResponse.setRevision(revision);
-		return retrieveListResponse;
+		RetrieveListResponse entity = new RetrieveListResponse();
+
+		entity.setId(id);
+		entity.setTitle(title);
+		entity.setOwner_type(ownerType);
+		entity.setOwner_id(ownerID);
+		entity.setList_type(listType);
+		entity.setPublic(isPublic);
+		entity.setRevision(revision);
+		entity.setCreated_at(createdAt);
+		entity.setCreated_by_request_id(created_by_request_id);
+		entity.setType(type);
+
+		return entity;
 	}
 
 	@Override
@@ -37,11 +47,15 @@ public class RetrieveListResponseTest extends AbstractRoundtripTest<RetrieveList
 		Assert.assertNotNull(expected);
 		Assert.assertNotNull(actual);
 		Assert.assertEquals(expected.getId(), actual.getId());
-		Assert.assertEquals(expected.getCreated_at(), actual.getCreated_at());
 		Assert.assertEquals(expected.getTitle(), actual.getTitle());
+		Assert.assertEquals(expected.getOwner_type(), actual.getOwner_type());
+		Assert.assertEquals(expected.getOwner_id(), actual.getOwner_id());
 		Assert.assertEquals(expected.getList_type(), actual.getList_type());
-		Assert.assertEquals(expected.getType(), actual.getType());
+		Assert.assertEquals(expected.isPublic(), actual.isPublic());
 		Assert.assertEquals(expected.getRevision(), actual.getRevision());
+		Assert.assertEquals(expected.getCreated_at(), actual.getCreated_at());
+		Assert.assertEquals(expected.getCreated_by_request_id(), actual.getCreated_by_request_id());
+		Assert.assertEquals(expected.getType(), actual.getType());
 	}
 
 }
