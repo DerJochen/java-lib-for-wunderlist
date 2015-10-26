4fc0af3355808285856112c6899b8c79497dcd89
Jochen Hormes <jochen@jochor.de>
2015-10-26 07:43:51 +0100

    Started work on the TaskService. #7

--- src/main/java/de/jochor/lib/wunderlist/service/TaskService.java
+++ src/main/java/de/jochor/lib/wunderlist/service/TaskService.java
@@ -1,7 +1,6 @@
 package de.jochor.lib.wunderlist.service;
 
 import de.jochor.lib.wunderlist.model.Authorization;
-import de.jochor.lib.wunderlist.model.RetrieveListResponse;
 import de.jochor.lib.wunderlist.model.Task;
 
 /**
@@ -17,13 +16,39 @@ import de.jochor.lib.wunderlist.model.Task;
 public interface TaskService {
 
 	/**
+	 * Retrieves all active {@link Task}s of a list.<br>
+	 * GET a.wunderlist.com/api/v1/tasks<br>
+	 * Params: list_id
+	 *
+	 * @param listID
+	 *            ID of the list
+	 * @param authorization
+	 *            {@link Authorization} containing client ID and access token
+	 * @return Array of {@link Task}s for the lists
+	 */
+	Task[] retrieveAll(int listID, Authorization authorization);
+
+	/**
+	 * Retrieves all completed {@link Task}s of a list.<br>
+	 * GET a.wunderlist.com/api/v1/tasks<br>
+	 * Params: list_id, completed
+	 *
+	 * @param listID
+	 *            ID of the list
+	 * @param authorization
+	 *            {@link Authorization} containing client ID and access token
+	 * @return Array of {@link Task}s for the lists
+	 */
+	Task[] retrieveAllCompleted(int listID, Authorization authorization);
+
+	/**
 	 * GET a.wunderlist.com/api/v1/tasks/:id
 	 *
 	 * @param id
 	 *            ID of the task to retrieve
 	 * @param authorization
 	 *            {@link Authorization} containing client ID and access token
-	 * @return Array of {@link RetrieveListResponse}s for all lists
+	 * @return Specified {@link Task}
 	 */
 	Task retrieve(int id, Authorization authorization);
 
